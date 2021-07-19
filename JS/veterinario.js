1c5c5865f94c654ea54a823c666697055c19bc9f
Edoardo paz <pazedoardo@gmail.com>
2021-07-19 08:43:19 -0300

    site 1.4

--- JS/veterinario.js
+++ JS/veterinario.js
@@ -13,3 +13,12 @@ var fecharM = document.getElementById('fechar-modal')
 fecharM.addEventListener('click' ,function() {
  document.getElementById('modal-vs').style.top = "-100%"
 })
+
+var menu = document.getElementById('menu-modal')
+menu.addEventListener('click', function () {
+    document.getElementById('menu').style.top = "29%"
+})
+var fecharmenu = document.getElementById('fecharmenu')
+fecharmenu.addEventListener('click', function () {
+    document.getElementById('menu').style.top = "-100%"
+})
